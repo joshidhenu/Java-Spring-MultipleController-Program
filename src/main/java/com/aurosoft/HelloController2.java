3e1f5db729a0aac32419106d5cf47653f01a0749
joshidhenu <dhenujoshi227@gmail.com>
2023-03-25 11:00:10 +0530

    first commit

---
+++ src/main/java/com/aurosoft/HelloController2.java
@@ -0,0 +1,15 @@
+package com.aurosoft;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController2 {
+	@RequestMapping("/hello2")
+	
+	public String dispaly()
+	{
+		return "viewpage2";
+	}
+
+}
